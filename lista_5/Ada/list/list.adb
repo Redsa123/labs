1d119e7cea08424917c9d135727a5aa4a47a726d
Redsa123 <serhiionopriienko510@gmail.com>
2025-12-16 23:43:15 +0100

    ada linked list and fixes in C

--- lista_5/Ada/list/list.adb
+++ lista_5/Ada/list/list.adb
@@ -1,64 +1,137 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body list is
-   function isEmpty (l : ListT) return Boolean is
-   begin
-      return l.first = null;
-   end isEmpty;
 
-   function Pop (l : in out ListT) return Integer is
-      n : NodePtr := l.first;
-      e : Integer := n.elem;
-   begin
-      l.first := n.next;
-      if l.first = null then -- last element
-         l.last := null;
-      end if;
-      Free (n);
-      return e;
-   end Pop;
+	function isEmpty (l : ListT) return Boolean is
+	begin
+		return l.first = null;
+	end isEmpty;
 
-   procedure Push (l : in out ListT; e : Integer) is
-      n : NodePtr := new Node;
-   begin
-      n.elem := e;
-      n.next := l.first;
-      l.first := n;
-      if l.last = null then -- first element
-         l.last := n;
-      end if;
-   end Push;
+	function Pop (l : in out ListT) return Integer is
+		n : NodePtr := l.first;
+		e : Integer;
+	begin
+		e := n.elem;
+		l.first := n.next;
+		if l.first = null then
+			l.last := null;
+		end if;
+		l.length := l.length - 1;
+		Free (n);
+		return e;
+	end Pop;
 
-   procedure Append (l : in out ListT; e : Integer) is
-      n : NodePtr := new Node;
-   begin
-      n.elem := e;
-      if l.first = null then -- first element
-         l.first := n;
-      else
-         l.last.next := n;
-      end if;
-      l.last := n;
-   end Append;
+	procedure Push (l : in out ListT; e : Integer) is
+		n : NodePtr := new Node;
+	begin
+		n.elem := e;
+		n.next := l.first;
+		l.first := n;
+		if l.last = null then
+			l.last := n;
+		end if;
+		l.length := l.length + 1;
+	end Push;
 
-   procedure Print (l : ListT) is
-      n : NodePtr := l.first;
-   begin
-      while n /= null loop
-         Put (n.elem'Image);
-         n := n.next;
-      end loop;
-      Put_Line (" (" & Length (l)'Image & " )");
-   end Print;
+	procedure Append (l : in out ListT; e : Integer) is
+		n : NodePtr := new Node;
+	begin
+		n.elem := e;
+		if l.first = null then
+			l.first := n;
+		else
+			l.last.next := n;
+		end if;
+		l.last := n;
+		l.length := l.length + 1;
+	end Append;
 
-   function Length (l : ListT) return Integer is
-      i : Integer := 0;
-      n : NodePtr := l.first;
-   begin
-      while n /= null loop
-         i := i + 1;
-         n := n.next;
-      end loop;
-      return i;
-   end Length;
+	function Get (l : ListT; index : Integer) return Integer is
+		n : NodePtr := l.first;
+	begin
+		for k in 1 .. index - 1 loop
+			n := n.next;
+		end loop;
+		return n.elem;
+	end Get;
+
+	procedure Put (l : in out ListT; index : Integer; e : Integer) is
+		n : NodePtr := l.first;
+	begin
+		for k in 1 .. index - 1 loop
+			n := n.next;
+		end loop;
+		n.elem := e;
+	end Put;
+
+	procedure Insert (l : in out ListT; index : Integer; e : Integer) is
+		n, new_node : NodePtr;
+	begin
+		if index = 1 then
+			Push (l, e);
+		elsif index = l.length + 1 then
+			Append (l, e);
+		else
+			n := l.first;
+			for k in 1 .. index - 2 loop
+				n := n.next;
+			end loop;
+			new_node := new Node;
+			new_node.elem := e;
+			new_node.next := n.next;
+			n.next := new_node;
+			l.length := l.length + 1;
+		end if;
+	end Insert;
+
+	procedure Delete (l : in out ListT; index : Integer) is
+		n, temp : NodePtr;
+	begin
+		if index = 1 then
+			declare
+				ignore : Integer;
+			begin
+				ignore := Pop (l);
+			end;
+		else
+			n := l.first;
+			for k in 1 .. index - 2 loop
+				n := n.next;
+			end loop;
+
+			temp := n.next;
+			n.next := temp.next;
+
+			if temp = l.last then
+				l.last := n;
+			end if;
+
+			Free (temp);
+			l.length := l.length - 1;
+		end if;
+	end Delete;
+
+	procedure Print (l : ListT) is
+		n : NodePtr := l.first;
+	begin
+		while n /= null loop
+			Put (n.elem'Image);
+			n := n.next;
+		end loop;
+		Put_Line (" (" & Length (l)'Image & " )");
+	end Print;
+
+	function Length (l : ListT) return Integer is
+	begin
+		return l.length;
+	end Length;
+
+	procedure Clean (l : in out ListT) is
+		temp : Integer;
+	begin
+		while not isEmpty (l) loop
+			temp := Pop (l);
+		end loop;
+	end Clean;
+
 end list;
--- lista_5/Ada/list/main.adb
+++ lista_5/Ada/list/main.adb
@@ -1,52 +1,123 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
+with Ada.Text_IO;                   use Ada.Text_IO;
+with Ada.Integer_Text_IO;           use Ada.Integer_Text_IO;
+with Ada.Strings.Unbounded;         use Ada.Strings.Unbounded;
 with Ada.Strings.Unbounded.Text_IO; use Ada.Strings.Unbounded.Text_IO;
 
 with list; use list;
 
-procedure listTest is
-   l : ListT;
-   r : Integer;
-   command : Unbounded_String;
-   continue : Boolean := True;
+procedure Main is
+	l        : ListT;
+	r        : Integer;
+	idx      : Integer;
+	command  : Unbounded_String;
+	continue : Boolean := True;
+	len      : Integer;
 begin
-   while continue loop
-      Put ("Command: ");
-      Get_Line (command);
-      if command = "Pop" then
-         if not isEmpty (l) then
-            r := Pop (l);
-            Put_Line ("Result: " & r'Image);
-         else
-            Put_Line ("Error - stack is empty!");
-         end if;
-      elsif command = "Push" then
-         Put ("Value: ");
-         Get (r);
-         Skip_Line;
-         Push (l, r);
-         Put_Line ("Result: OK");
-      elsif command = "Append" then
-         Put ("Value: ");
-         Get (r);
-         Skip_Line;
-         Append (l, r);
-         Put_Line ("Result: OK");
-      elsif command = "Print" then
-         Put ("Result: ");
-         Print (l);
-      elsif command = "Length" then
-         r := Length (l);
-         Put_Line ("Result: " & r'Image);
-      elsif command = "Exit" then
-         continue := False;
-      else
-         Put_Line ("Unknown command!");
-      end if;
-   end loop;
-   --  clean list
-   while not isEmpty (l) loop
-      r := Pop (l);
-   end loop;
-end listTest;
+	Put_Line ("--- Created a new list ---");
+	Put_Line
+	  ("Available commands: pop, push, print, append, get, put, insert, delete, clean, exit");
+	while continue loop
+		Put ("Command: ");
+		Get_Line (command);
+
+		if command = "Pop" then
+			if not isEmpty (l) then
+				r := Pop (l);
+				Put_Line ("Result: " & r'Image);
+			else
+				Put_Line ("Error - stack is empty!");
+			end if;
+
+		elsif command = "Push" then
+			Put ("Value: ");
+			Get (r);
+			Skip_Line;
+			Push (l, r);
+			Put_Line ("Result: OK");
+
+		elsif command = "Append" then
+			Put ("Value: ");
+			Get (r);
+			Skip_Line;
+			Append (l, r);
+			Put_Line ("Result: OK");
+
+		elsif command = "Insert" then
+			Put ("Index: ");
+			Get (idx);
+			Put_Line ("");
+			Put ("Value: ");
+			Get (r);
+			Put_Line ("");
+
+			len := Length (l);
+			if idx >= 1 and idx <= len + 1 then
+				Insert (l, idx, r);
+				Put_Line ("Result: OK");
+			else
+				Put_Line ("Error - bad index!");
+			end if;
+
+		elsif command = "Delete" then
+			Put ("Index: ");
+			Get (idx);
+			Skip_Line;
+
+			len := Length (l);
+			if idx >= 1 and idx <= len then
+				Delete (l, idx);
+				Put_Line ("Result: OK");
+			else
+				Put_Line ("Error - bad index!");
+			end if;
+
+		elsif command = "Get" then
+			Put ("Index: ");
+			Get (idx);
+			Skip_Line;
+
+			len := Length (l);
+			if idx >= 1 and idx <= len then
+				r := Get (l, idx);
+				Put_Line ("Result: " & r'Image);
+			else
+				Put_Line ("Error - bad index!");
+			end if;
+
+		elsif command = "Put" then
+			Put ("Index: ");
+			Get (idx);
+			Skip_Line;
+			Put ("Value: ");
+			Get (r);
+			Skip_Line;
+
+			len := Length (l);
+			if idx >= 1 and idx <= len then
+				list.Put (l, idx, r);
+				Put_Line ("Result: OK");
+			else
+				Put_Line ("Error - bad index!");
+			end if;
+
+		elsif command = "Print" then
+			Put ("Result: ");
+			Print (l);
+
+		elsif command = "Length" then
+			r := Length (l);
+			Put_Line ("Result: " & r'Image);
+
+		elsif command = "Clean" then
+			Clean (l);
+			Put_Line ("Result: OK");
+
+		elsif command = "Exit" then
+			continue := False;
+		else
+			Put_Line ("Unavailable command yet");
+		end if;
+	end loop;
+
+	Clean (l);
+end Main;
